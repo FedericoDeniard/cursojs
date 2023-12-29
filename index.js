d9a7a508b2a5c650116e4b6486b612c640cb52c7
Federico Deniard <fededeniard@gmail.com>
2023-12-29 18:18:24 -0300

    text shadow

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const articleBackground = document.getElementsByClassName('background')[0];
     const evenCards = document.getElementsByClassName ('even');
     const oddCards = document.getElementsByClassName('odd');
+    const evenCardsText = document.getElementsByClassName('evenText')
+    console.log(evenCardsText)
 
     const theme = document.getElementById("toggle");
 
@@ -16,6 +18,10 @@ document.addEventListener('DOMContentLoaded', function() {
             for (let i = 0; i < oddCards.length; i++){
                 oddCards[i].style.backgroundColor = "#FFD54F"
             }
+            for (let i = 0; i < evenCardsText.length; i++){
+                evenCardsText[i].style.color = "#fff"
+                evenCardsText[i].style.textShadow = "1px 1px 2px #000"
+            }
         } else {
             articleBackground.style.backgroundColor = "#fff";
             for (let i = 0; i < evenCards.length; i++){
@@ -23,6 +29,10 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             for (let i = 0; i < oddCards.length; i++){
                 oddCards[i].style.backgroundColor = "#FFE4B5"
+            }
+            for (let i = 0; i < evenCardsText.length; i++){
+                evenCardsText[i].style.color = "#000"
+                evenCardsText[i].style.textShadow = "1px 1px 2px #fff"
             }
         }
     });
