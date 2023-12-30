231e3244309bddbb67b98b40e23507c15358bd59
Federico Deniard <fededeniard@gmail.com>
2023-12-30 01:00:37 -0300

    -dark theme color scheme changed

--- index.js
+++ index.js
@@ -2,25 +2,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const articleBackground = document.getElementsByClassName('background')[0];
     const evenCards = document.getElementsByClassName ('even');
     const oddCards = document.getElementsByClassName('odd');
-    const evenCardsText = document.getElementsByClassName('evenText')
-    console.log(evenCardsText)
-
+    const cardsText = document.getElementsByClassName('text');
+    const cardsTitle = document.getElementsByClassName('cardTitle');
     const theme = document.getElementById("toggle");
+    const images = document.getElementsByClassName('image');
+    const headerFooter = document.getElementsByClassName('headerFooter');
 
     theme.addEventListener('click', () => {
         const toggleValue = theme.checked;
 
         if (toggleValue) {
-            articleBackground.style.backgroundColor = "#6C6C6C";
+            articleBackground.style.backgroundColor = "#2B2B2B";
             for (let i = 0; i < evenCards.length; i++){
-                evenCards[i].style.backgroundColor = "#795548"
+                evenCards[i].style.backgroundColor = "#171010"
             }
             for (let i = 0; i < oddCards.length; i++){
-                oddCards[i].style.backgroundColor = "#FFD54F"
+                oddCards[i].style.backgroundColor = "#423F3E"
             }
-            for (let i = 0; i < evenCardsText.length; i++){
-                evenCardsText[i].style.color = "#fff"
-                evenCardsText[i].style.textShadow = "1px 1px 2px #000"
+            for (let i = 0; i < cardsText.length; i++){
+                cardsText[i].style.color = "#fff"
+                cardsText[i].style.textShadow = "1px 1px 2px #000"
+            }
+            for (let i = 0; i < cardsTitle.length; i++){
+                cardsTitle[i].style.color = "#CCCCCC ";
+            }
+            for (let i = 0; i < images.length; i++){
+                images[i].style.boxShadow = "5px 5px 10px 2px #2B2B2B"
+            }
+            for (let i = 0; i < headerFooter.length; i++){
+                headerFooter[i].style.backgroundColor = "#362222";
             }
         } else {
             articleBackground.style.backgroundColor = "#fff";
@@ -30,9 +40,18 @@ document.addEventListener('DOMContentLoaded', function() {
             for (let i = 0; i < oddCards.length; i++){
                 oddCards[i].style.backgroundColor = "#FFE4B5"
             }
-            for (let i = 0; i < evenCardsText.length; i++){
-                evenCardsText[i].style.color = "#000"
-                evenCardsText[i].style.textShadow = "1px 1px 2px #fff"
+            for (let i = 0; i < cardsText.length; i++){
+                cardsText[i].style.color = "#000"
+                cardsText[i].style.textShadow = "1px 1px 2px #fff"
+            }
+            for (let i = 0; i < cardsTitle.length; i++){
+                cardsTitle[i].style.color = "#000";
+            }
+                 for (let i = 0; i < images.length; i++){
+                images[i].style.boxShadow = "5px 5px 10px 2px #000"
+            }
+            for (let i = 0; i < headerFooter.length; i++){
+                headerFooter[i].style.backgroundColor = "#ffcd28";
             }
         }
     });
