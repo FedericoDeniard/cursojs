8d8ccbdec32cc36c2932d1fff4c3b52eace75b83
Federico Deniard <fededeniard@gmail.com>
2023-12-30 18:57:32 -0300

    Random notes project finished

---
+++ pages/random-notes/app.js
@@ -0,0 +1,19 @@
+let botonElem = document.getElementById('boton-cambiar-cita');
+let citaElem = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function randomNumber(min,max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function cambiarCita(){
+    let indiceAleatorio = randomNumber(0, quotes.length);
+    citaElem.innerText = `"${quotes[indiceAleatorio].texto}"`;
+    autorElem.innerText = quotes[indiceAleatorio].autor;
+
+}
+
+
+cambiarCita();
+
+botonElem.addEventListener('click', cambiarCita);
---
+++ pages/random-notes/quotes.js
@@ -0,0 +1,163 @@
+let quotes = [
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¡Chupa, chupa, cinco dolareh!'
+    },
+    {
+        'autor': 'Sr. Garrison',
+        'texto': '¡Hey tú! Marica de mierda, marica de mierda, ¿cómo te va?'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¡Kenny, puto pobre de mierda!'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¡No me llames gordo de mierda!… soy una mierda fuerte.'
+    },
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': '¡No ofendas a mi gente culo gordo!'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¡No viviré mi vida como una jodida minoría! '
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¡Solo hay tres sospechosos: ¡Kyle por judío, Token por negro y Kenny por pobre!  '
+    },
+    {
+        'autor': 'Chef',
+        'texto': '¿Es una sandez mayor aceptar que Dios envió a su hijo a morir por nuestros pecados o que Buda se pasó 20 años atado a un árbol?'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': '¿Podríamos pasar por condones? Porque si vas a joderme, al menos quisiera estar protegido.'
+    },
+    {
+        'autor': 'Sr.Garrison',
+        'texto': 'Ahora los heteros se comportan como si fueran más gais que yo. ¿Qué hicisteis los negros cuando los blancos os robaron vuestra cultura?'
+    },
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': 'Cada vez que tengo una buena idea, el gobierno la arruina.'
+    },
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': 'Cállate culo Gordo.'
+    },
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': 'Cartman tiene razón por la razón errónea y nosotros no por la razón correcta.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Chupa mis bolas Kyle.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Creo que Kyle tiene arena en la vagina.'
+    },
+    {
+        'autor': 'Stan Marsh',
+        'texto': 'En la vida hay grandes interrogantes. ¿Por qué estamos aquí? ¿De dónde venimos? Pero si la gente sigue creyendo en ojetes de mierda como tú, nunca sabrán la respuesta. No sólo mientes, sino que retrasas el progreso de la humanidad.'
+    },
+    {
+        'autor': 'Chef',
+        'texto': 'Encuentra el Clítoris, él te ayudará.'
+    },
+    {
+        'autor': 'Stan Marsh',
+        'texto': 'Eres una mierda y te nomino al premio de la mayor mierda del mundo.'
+    },
+    {
+        'autor': 'Chef',
+        'texto': 'Hay un lugar y un momento para todo y es… LA UNIVERSIDAD.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Hippies, están por todos lados. Dicen que quieren salvar al mundo, pero lo único que hacen es fumar marihuana y oler mal.'
+    },
+    {
+        'autor': 'Toallín',
+        'texto': 'Hoy aprendí algo, no debería drogarme para tener ideas, debería tener ideas y drogarme para premiarme a mí mismo.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Jodeos, jodeos y jodeos.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'La madre de Kyle es una puta.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'La próxima vez le diré al pedo que se aguante más tiempo en el culo antes de salir.'
+    },
+
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Las drogas son malas porque si las tomas eres un hippie y los hippies son unos apestosos.'
+    },
+
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': 'Los animales matan, y los que no lo hacen son animales estúpidos, como las tortugas y demás.'
+    },
+
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Los judíos no tienen alma.'
+    },
+
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Me cago en la puta judía que te parió.'
+    },
+
+    {
+        'autor': 'Stan Marsh',
+        'texto': 'No entiendo nada de esto, necesito que venga Morgan Freeman a explicarme.'
+    },
+
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Qué mal que beber Whisky no sea un trabajo porque el papá de Kenny sería millonario.'
+    },
+
+    {
+        'autor': 'Sr. Garrison',
+        'texto': 'Recuerden, niños, no hay preguntas estúpidas, sólo gente estúpida.'
+    },
+
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Si Mel Gibson dice que los judíos son malos, ¡es porque ES CIERTO!'
+    },
+
+    {
+        'autor': 'Sr. Garrison',
+        'texto': 'Sinceramente no confío en nada que sangre por 5 días y no muera.'
+    },
+    {
+        'autor': 'Randy Marsh',
+        'texto': 'Sólo estoy dándome algo de cáncer, dile a tu mamá que no se preocupe.'
+    },
+    {
+        'autor': 'Stan Marsh',
+        'texto': '¡¡Oh Dios mío!!, ¡han matado a Kenny!'
+    },
+    {
+        'autor': 'Kyle Broflovski',
+        'texto': ' ¡¡Hijos de puta!!'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Te mataré judíamente Kyle.'
+    },
+    {
+        'autor': 'Eric Cartman',
+        'texto': 'Tu mamá es tan pobre que se embarazó para sentir algo en la barriga.'
+    }
+]
